0e0299c0184e333293c4f24e2b7213d77205c44d
Laboratorios <Laboratorios@LAB202-22>
2018-09-24 17:06:45 -0300

    Alterações na classe Main

--- Aula06_ED/src/view/Main.java
+++ Aula06_ED/src/view/Main.java
@@ -8,11 +8,11 @@ public class Main {
 	public static void main(String[] args) throws EmptyStackException {
 		
 		ChecaFormacao c = new ChecaFormacao();
-		String texto = "([{<>}])";//Texto com abertura e fechamento de caracteres especiais
+		String texto = "(]";//Texto com abertura e fechamento de caracteres especiais
 		System.out.println(c.checaFormacao(texto) ? "Formação perfeita !!!" : "Formação imperfeita !!!");
 		
-		texto = "<div class='container'></div>";//Exemplo de aplicação desta checagem, neste caso esta sendo checado se as tags foram fechadas
-		System.out.println(c.checaFormacao(texto) ? "Formação perfeita !!!" : "Formação imperfeita !!!");
+		String texto2 = "<div class='container'></div>";//Exemplo de aplicação desta checagem, neste caso esta sendo checado se as tags foram fechadas
+		System.out.println(c.checaFormacao(texto2) ? "Formação perfeita !!!" : "Formação imperfeita !!!");
 		
 		texto = "((((()";//Texto com abertura, mas sem fechamento de caracteres especiais
 		System.out.println(c.checaFormacao(texto) ? "Formação perfeita !!!" : "Formação imperfeita !!!");
